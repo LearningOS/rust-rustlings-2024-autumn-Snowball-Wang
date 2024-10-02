8b59310e995a5e665835dec8f3d7858c52246320
Jieqiang Wang <wjq1996@mail.ustc.edu.cn>
2024-10-02 23:57:43 +0100

    complete hashmap exercises

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,9 +35,16 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
+        // Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        match fruit {
+            Fruit::Apple => basket.entry(Fruit::Apple).or_insert(2),
+            Fruit::Banana => basket.entry(Fruit::Banana).or_insert(2),
+            Fruit::Mango => basket.entry(Fruit::Mango).or_insert(2),
+            Fruit::Lychee => basket.entry(Fruit::Lychee).or_insert(2),
+            Fruit::Pineapple => basket.entry(Fruit::Pineapple).or_insert(2),
+        };
     }
 }
 
@@ -81,7 +86,7 @@ mod tests {
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
-    
+
     #[test]
     fn all_fruit_types_in_basket() {
         let mut basket = get_fruit_basket();
